ce2167b76b5d7da544521d7d40160065ca98ac62
Rana Waqar Ahmad <ranawaqarahmad33@gmail.com>
2021-09-01 23:02:10 +0500

    Commit Initial

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect, useCallback } from "react";
+import AddMovie from "./components/AddMovie";
+import MovieList from "./components/MovieList";
+import "./App.css";
+
+function App() {
+  const [movies, SetMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-a74d1-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText
+        });
+      }
+      console.log(loadedMovies);
+      SetMovies(loadedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch(
+      "https://react-http-a74d1-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+  let content = <p>Found No Movie</p>;
+
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading....</p>;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </React.Fragment>
+  );
+}
+
+export default App;
